c8d1d1f8db4ccfa7ab9cff89024812ac2804fa22
goodmusic134 <144806074+goodmusic134@users.noreply.github.com>
2023-09-13 21:18:17 +0530

    Update _autobio.js

--- plugins/_autobio.js
+++ plugins/_autobio.js
@@ -12,7 +12,7 @@ let _muptime
       }) * 1000
     }
     let muptime = clockString(_muptime)
-		let bio = `\n🟢 Time Active ${muptime}\n\n ┃ 💎  By GURU`
+		let bio = `මහින්ද මහත්තයා ❤`
 		await this.updateProfileStatus(bio).catch(_ => _)
 		setting.status = new Date() * 1
 
